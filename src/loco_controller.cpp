094bf774e49cc2afb12582787185ae4a38c9cc52
Toprak Efe Akkılıç <efe.akkilic@ozu.edu.tr>
2024-06-04 18:57:20 +0000

    Working.

--- src/loco_controller.cpp
+++ src/loco_controller.cpp
@@ -9,8 +9,11 @@
  * 
  */
 
+#include <mutex>
 #include <ros/ros.h>
 #include <tmotor.hpp>
+#include <tmotor/MotorCommand.h>
+#include <tmotor/MotorFeedback.h>
 #include <geometry_msgs/Twist.h>
 
 /* I am doing it with macros, because in all due honesty,
@@ -19,44 +22,78 @@
   physical model of our robot and the simplest of things like
   moment of inertia. No URDF for the rover itself, I don't care
   if our model isn't good, the closed-loop control IS good enough. */
-#define WHEEL_RADIUS 0.1425 // cm
+#define WHEEL_RADIUS 0.1425f // cm
+#define ROBOT_WIDTH 0.75f // cm
 
 class LocoControllerNode {
 public:
-  LocoControllerNode() :
-    m_nh("~"),
-    m_motors({
-      {"front_left", TMotor::AKManager(1)},
-      {"front_right", TMotor::AKManager(2)},
-      {"rear_left", TMotor::AKManager(3)},
-      {"rear_right", TMotor::AKManager(4)}
-    })
-  {
-    m_sub = m_nh.subscribe("cmd_vel", 1, &LocoControllerNode::cmdVelCallback, this);
+  LocoControllerNode() {
+    m_active_twist.linear.x = 0.0f;
+    m_active_twist.linear.y = 0.0f;
+    m_active_twist.linear.z = 0.0f;
+    m_active_twist.angular.z = 0.0f;
+    m_active_twist.angular.y = 0.0f;
+    m_active_twist.angular.z = 0.0f;
+    m_pub_fl = m_nh.advertise<tmotor::MotorCommand>("front_right/motor_command", 1);
+    m_pub_fr = m_nh.advertise<tmotor::MotorCommand>("front_left/motor_command", 1);
+    m_pub_rl = m_nh.advertise<tmotor::MotorCommand>("rear_right/motor_command", 1);
+    m_pub_rr = m_nh.advertise<tmotor::MotorCommand>("rear_left/motor_command", 1);
+    std::thread control_thread([this]{
+      ros::Rate freq(20); // 20hz
+      while (ros::ok()) {
+      float linear, angular;
+      {
+        std::lock_guard<std::mutex> lock(m_mutex);
+        linear = m_active_twist.linear.x;
+        angular = m_active_twist.angular.z;
+        m_active_twist.linear.x *= 0.95f;
+      }
+      if (linear == 0.0f) continue;
+      float v_l = (std::abs(linear) - angular * ROBOT_WIDTH)/WHEEL_RADIUS;
+      float v_r = (std::abs(linear) + angular * ROBOT_WIDTH)/WHEEL_RADIUS;
+      const bool forward = linear > 0.0f;
+      v_l = forward ? v_l : -v_l;
+      v_r = forward ? v_r : -v_r;
+      m_active_cmd[0].velocity = v_r;
+      m_active_cmd[1].velocity = v_l;
+      m_active_cmd[3].velocity = v_r;
+      m_active_cmd[4].velocity = v_l;
+      m_pub_fr.publish(m_active_cmd[0]);
+      m_pub_fl.publish(m_active_cmd[1]);
+      m_pub_rr.publish(m_active_cmd[2]);
+      m_pub_rl.publish(m_active_cmd[3]);
+      freq.sleep();
+    }});
+    control_thread.detach();
   }
 
-  void init() {
-    const std::string can_interface = m_nh.param<std::string>("can_interface", "vcan0");
-    for (auto& motor : m_motors) {
-      motor.second.connect(can_interface.c_str());
-    }
+  void run() {
+    ros::spin();
   }
 
 private:
   std::map<std::string, TMotor::AKManager> m_motors;
   ros::NodeHandle m_nh;
-  ros::Subscriber m_sub;
+  ros::Publisher m_pub_fr;
+  ros::Publisher m_pub_fl;
+  ros::Publisher m_pub_rr;
+  ros::Publisher m_pub_rl;
+  std::mutex m_mutex;
+  geometry_msgs::Twist m_active_twist;
+  tmotor::MotorCommand m_active_cmd[4];
+
+  void controlLoop() {
+    
+  }
 
   void cmdVelCallback(const geometry_msgs::TwistConstPtr& msg) {
-    float linear = msg->linear.x;
-    float angular = msg->angular.z;
-    
+    m_active_twist.linear.x = msg->linear.x;
+    m_active_twist.angular.z = msg->angular.z;
   }
 };
 
 int main(int argc, char* argv[]) {
-  //LocoControllerNode node;
   ros::init(argc, argv, "loco_controller");
-  //node.init();
-  ros::spin();
+  LocoControllerNode node;
+  node.run();
 }
---
+++ src/wheel_controller.cpp
@@ -0,0 +1,147 @@
+/**
+ * @file wheel_controller.cpp
+ * @author Toprak Efe Akkılıç (efe.akkilic@ozu.edu.tr)
+ * @brief 
+ * @version 0.1
+ * @date 2024-06-04
+ * 
+ * @copyright Copyright (c) 2024
+ * 
+ */
+
+#include <mutex>
+#include <thread>
+#include <ros/ros.h>
+#include <functional>
+#include <signal.h>
+#include <tmotor.hpp>
+#include <tmotor/MotorCommand.h>
+#include <tmotor/MotorFeedback.h>
+
+sig_atomic_t volatile request_shutdown = 0;
+
+class MotorNode
+{
+public:
+  MotorNode(int id) :
+    n_nh("~"),
+    motor_manager(id),
+    vel_cmd(5.0),
+    pose_cur(0.0), vel_cur(0.0), eff_cur(0.0),
+    mode_id(TMotor::MotorModeID::VELOCITY)
+  {
+    std::string can_interface;
+    if (!ros::param::get("can_interface", can_interface)) {
+      ROS_ERROR("CAN interface not provided, exiting...");
+      ros::shutdown();
+    }
+    ROS_INFO("Connecting to CAN interface %s", can_interface.c_str());
+    motor_manager.connect(can_interface.c_str());
+    n_nh = ros::NodeHandle("~");
+    n_sub = n_nh.subscribe("motor_command", 1, &MotorNode::motorCommandCallback, this);
+    n_pub = n_nh.advertise<tmotor::MotorFeedback>("motor_feedback", 1);
+  } 
+
+  void run() {
+    motor_updater = std::thread([this] {
+      ros::Rate loop_rate(20);
+      while (!request_shutdown && ros::ok()) {
+        tmotor::MotorFeedback msg;
+        setMotorStates(motor_manager.getPosition(), motor_manager.getVelocity(), motor_manager.getCurrent());
+        msg.position = getPoseState();
+        msg.velocity = getVelocityState();
+        msg.current = getEffortState();
+        msg.temperature = motor_manager.getTemperature();
+        msg.motor_fault = motor_manager.getFault();
+        if (msg.motor_fault != TMotor::MotorFault::NONE) {
+          std::string warning_msg = "Motor fault detected for ID " + std::to_string(motor_manager.getMotorID()) + " with fault: " + fault_to_str(motor_manager.getFault());
+          ROS_WARN("%s", warning_msg.c_str());
+        }
+        n_pub.publish(msg);
+        motor_manager.sendVelocity(getVelocityCommand());
+        loop_rate.sleep();
+      }
+    });
+
+    motor_updater.detach();
+    ros::spin();
+  }
+
+private:
+  float vel_cmd;
+  float pose_cur, vel_cur, eff_cur;
+  std::mutex motor_mutex;
+  std::thread motor_updater;
+  TMotor::AKManager motor_manager;
+  ros::NodeHandle n_nh;
+  ros::Subscriber n_sub;
+  ros::Publisher n_pub;
+  TMotor::MotorModeID mode_id;
+
+  float getVelocityCommand() {
+    std::lock_guard<std::mutex> lock(motor_mutex);
+    return vel_cmd;
+  }
+
+  void setVelocityCommand(float vel) {
+    std::lock_guard<std::mutex> lock(motor_mutex);
+    vel_cmd = vel;
+  }
+
+  float getPoseState() {
+    std::lock_guard<std::mutex> lock(motor_mutex);
+    return pose_cur;
+  }
+
+  float getVelocityState() {
+    std::lock_guard<std::mutex> lock(motor_mutex);
+    return vel_cur;
+  }
+
+  float getEffortState() {
+    std::lock_guard<std::mutex> lock(motor_mutex);
+    return eff_cur;
+  }
+
+  void setMotorStates(float pose, float vel, float eff) {
+    std::lock_guard<std::mutex> lock(motor_mutex);
+    pose_cur = pose;
+    vel_cur = vel;
+    eff_cur = eff;
+  }
+
+  void motorCommandCallback(const tmotor::MotorCommand::ConstPtr& msg) {
+    static const double vel_max = 22.5;
+    static const double vel_min = -22.5;
+
+    std::lock_guard<std::mutex> lock(motor_mutex);
+    mode_id = static_cast<TMotor::MotorModeID>(msg->type);
+    vel_cmd = msg->velocity < vel_max ? msg->velocity : vel_max;
+    vel_cmd = msg->velocity > vel_min ? msg->velocity : vel_min;
+  }
+
+  std::string fault_to_str(TMotor::MotorFault fault) {
+    static std::map<TMotor::MotorFault, std::string> dict({
+      {TMotor::MotorFault::NONE,            "NONE"           },
+      {TMotor::MotorFault::OVERTEMPERATURE, "OVERTEMPERATURE"},
+      {TMotor::MotorFault::OVERCURRENT,     "OVERCURRENT"    },
+      {TMotor::MotorFault::OVERVOLTAGE,     "OVERVOLTAGE"    },
+      {TMotor::MotorFault::UNDERVOLTAGE,    "UNDERVOLTAGE"   },
+      {TMotor::MotorFault::ENCODER,         "ENCODER"        },
+      {TMotor::MotorFault::HARDWARE,        "HARDWARE"       }
+    });
+    return dict[fault];
+  }
+
+};
+
+int main(int argc, char** argv) {
+  ros::init(argc, argv, "tmotor_node");
+  int id;
+  ros::param::get("~id", id);
+  ROS_INFO("AK motor node started with id %d", id);
+  signal(SIGINT, [](int sig) { request_shutdown = 1; });
+  MotorNode node(id);
+  node.run();
+  return 0;
+}
