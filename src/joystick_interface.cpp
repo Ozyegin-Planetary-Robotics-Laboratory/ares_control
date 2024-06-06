1b3d32fbf739de8259fb5882fea8ef5aa222c72a
Toprak Efe Akkılıç <efe.akkilic@ozu.edu.tr>
2024-06-06 13:56:19 +0000

    Added speed config.

--- src/joystick_interface.cpp
+++ src/joystick_interface.cpp
@@ -3,6 +3,8 @@
 #include <geometry_msgs/Twist.h>
 
 ros::Publisher pub;
+float linear_scale = 1.0;
+float angular_scale = 1.0;
 
 void clamp(double& val, double min, double max) {
   if (val < min) val = min;
@@ -13,8 +15,8 @@ void joyCallback(const sensor_msgs::Joy::ConstPtr& joy) {
   geometry_msgs::Twist twist;
   twist.linear.x = joy->axes[1];
   twist.angular.z = joy->axes[0];
-  clamp(twist.linear.x, -1.0, 1.0);
-  clamp(twist.angular.z, -1.0, 1.0);
+  clamp(twist.linear.x, -linear_scale, linear_scale);
+  clamp(twist.angular.z, -angular_scale, angular_scale);
   pub.publish(twist);
 }
 
@@ -22,6 +24,8 @@ int main(int argc, char** argv) {
   ros::init(argc, argv, "joystick_interface");
   ros::NodeHandle nh;
   ros::Subscriber sub;
+  nh.getParam("joystick_control/linear_scale", linear_scale);
+  nh.getParam("joystick_control/angular_scale", angular_scale);
   pub = nh.advertise<geometry_msgs::Twist>("cmd_vel", 1);
   sub = nh.subscribe("joy", 1, joyCallback);
   ros::spin();
