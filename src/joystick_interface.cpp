6d910ace0485f3ac08d01ef6d1aa92a68dc45b3e
Toprak Efe Akkılıç <efe.akkilic@ozu.edu.tr>
2024-06-06 14:28:02 +0000

    Adjusting.

--- src/joystick_interface.cpp
+++ src/joystick_interface.cpp
@@ -6,36 +6,29 @@
 ros::Publisher pub;
 double linear_scale, angular_scale;
 geometry_msgs::Twist twist;
-std::mutex twist_mutex;
-
-void publish() {
-  std::lock_guard<std::mutex> lock(twist_mutex);
-  pub.publish(twist);
-}
 
 void joyCallback(const sensor_msgs::Joy::ConstPtr& joy) {
   geometry_msgs::Twist twist;
-  std::lock_guard<std::mutex> lock(twist_mutex);
   twist.linear.x = joy->axes[1]*linear_scale;
   twist.angular.z = joy->axes[0]*angular_scale;
 }
 
 int main(int argc, char** argv) {
+  ros::NodeHandle nh;
+  ros::Subscriber sub;
+  
   twist.angular.x = twist.angular.y = twist.linear.y = twist.linear.z = 0;
   twist.linear.x = twist.linear.y = twist.linear.z = 0;
+
   ros::init(argc, argv, "joystick_interface");
-  ros::NodeHandle nh;
   nh.getParam("joystick_control/linear_coeff", linear_scale);
   nh.getParam("joystick_control/angular_coeff", angular_scale); 
-  ros::Subscriber sub;
   pub = nh.advertise<geometry_msgs::Twist>("cmd_vel", 1);
   sub = nh.subscribe("joy", 1, joyCallback);
-  ros::AsyncSpinner spinner(1);
-  spinner.start();
   ros::Rate loop_rate(10);
   while (ros::ok()) {
-    publish();
+    pub.publish(twist);
+    ros::spinOnce();
     loop_rate.sleep();
   }
-  spinner.stop();
 }
