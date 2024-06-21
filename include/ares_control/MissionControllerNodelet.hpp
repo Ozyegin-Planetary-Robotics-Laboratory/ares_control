c8b2279798d764a9e67b749ed9f6f4a197240493
Toprak-Efe <efe.akkilic@ozu.edu.tr>
2024-06-21 11:40:48 +0000

    Added scaffold for mission controller nodelet.

---
+++ include/ares_control/MissionControllerNodelet.hpp
@@ -0,0 +1,19 @@
+#ifndef MISSION_CONTROLLER_NODELET_HPP
+#define MISSION_CONTROLLER_NODELET_HPP
+
+#include <ros/ros.h>
+#include <nodelet/nodelet.h>
+
+namespace ares_control
+{
+  class MissionControllerNodelet : public nodelet::Nodelet 
+  {
+  public:
+
+  private:
+  
+  }; // class MissionControllerNodelet
+} // namespace ares_control
+
+
+#endif // MISSION_CONTROLLER_NODELET_HPP
